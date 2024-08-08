9b9d3bc7e5d1f507ee159bf323b617c70a7f1017
Danil <dabilsch60@gmail.com>
2024-08-08 22:29:57 +0300

    try1

---
+++ src/main/java/org/example/UserName.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class UserName {
+    private String lastname;
+    private String name;
+
+    public UserName(String lastname, String name) {
+        this.lastname = lastname;
+        this.name = name;
+    }
+
+}
+
